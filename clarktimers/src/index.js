f63a0a471fe8f0472244d1e5644f3cf89082fded
clarkgh <demolisho@hotmail.com>
2017-09-06 16:14:27 -0700

    set up react clock

--- clarktimers/src/index.js
+++ clarktimers/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function renderApp() {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+}
+
+setInterval(renderApp, 1000);
