f6c2134a7442b10181a4102b60452162da6830dc
clarkgh <demolisho@hotmail.com>
2017-09-07 08:25:57 -0700

    Removed uneccessary renderApp function

--- clarkclock/src/index.js
+++ clarkclock/src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function renderApp() {
-  ReactDOM.render(<App />, document.getElementById('root'));
-  registerServiceWorker();
-}
-
-renderApp();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
