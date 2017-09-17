4ef82e00abac84b6327f10fd03279ffea6065faf
clarkgh <demolisho@hotmail.com>
2017-09-17 12:53:07 -0700

    first react component

---
+++ redditAPI/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return ( <div>Check it out now. The funky react setup!</div> );
+   }
+}
+
+export default App;
