6b80d897a7d42561846429c9f993c076ef1a8fee
clarkgh <demolisho@hotmail.com>
2017-09-17 12:59:09 -0700

    more spacing fixes

--- redditAPI/App.jsx
+++ redditAPI/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return ( <div>Check it out now. The funky react setup!</div> );
-   }
+  render() {
+    return ( <div>Check it out now. The funky react setup!</div> );
+  }
 }
 
 export default App;
