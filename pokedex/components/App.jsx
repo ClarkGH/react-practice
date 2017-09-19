916c06e1abe76e1d28d0576fa5a72a4e528a2bdb
clarkgh <demolisho@hotmail.com>
2017-09-19 13:04:45 -0700

    API connection good, next build object and save what I want to keep

--- pokedex/components/App.jsx
+++ pokedex/components/App.jsx
@@ -5,6 +5,7 @@ import Pokedex from '../pokedex';
 
 class App extends React.Component {
   render() {
+    Pokedex( 1 );
     return ( <div>Check it out now. The funky react setup!</div> );
   }
 }
--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 const ROOT_URL = 'http://pokeapi.co/api/v2/pokemon/';
 
-module.exports = ( options, callback ) => {
-  axios.get( ROOT_URL )
-    .then( ( response ) => {
-      console.log( response );
-    })
-    .catch( (error) => {
-      console.log( error );
-    });
+module.exports = ( pokemonNum ) => {
+  var isRealPokemon = ( typeof pokemonNum === 'number' && pokemonNum <= 811 && pokemonNum >= 1 );
+
+  // if ( isRealPokemon ) {
+  //   axios.get( ROOT_URL + pokemonNum )
+  //     .then( ( response ) => {
+  //       console.log( response.data );
+  //     })
+  //     .catch( (error) => {
+  //       console.log( error );
+  //     });
+  // } else {
+  //   console.error('Error: No true pokemon number specified')
+  // }
 };
