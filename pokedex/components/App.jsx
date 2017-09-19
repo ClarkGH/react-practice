3c9cbbc86843475d16099270a744154a84b28ddc
clarkgh <demolisho@hotmail.com>
2017-09-19 14:29:53 -0700

    style inclusion, will pretty up later, potentially add sass

--- pokedex/components/App.jsx
+++ pokedex/components/App.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return ( 
-      <div className="Pokemon">
+      <div className="pokemon-info">
         <h1>Pokedex</h1> 
         <h2>Name: Bulbasaur</h2>
       </div> 
