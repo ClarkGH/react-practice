8f6fd325cb309fceb1ca47695ce5a4b10dd6b667
clarkgh <demolisho@hotmail.com>
2017-09-20 20:57:35 -0700

    pokedex working, next make it dynamic

--- pokedex/components/App.jsx
+++ pokedex/components/App.jsx
@@ -8,7 +8,11 @@ class App extends React.Component {
     super( props );
 
     this.state = {
-      selectedPokemon: null
+      selectedPokemon: 'Bulbasaur',
+      pokemonId: 1,
+      pokemonType1: "Grass",
+      pokemonType2: "Poison",
+      pokemonImgUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
     }
 
     this.selectPokemon( 1 );
@@ -17,7 +21,11 @@ class App extends React.Component {
   selectPokemon( pokedexNum ) {
     Pokedex( 1, ( pokemonData ) => {
       this.setState({
-        selectedPokemon: pokemonData.name
+        selectedPokemon: pokemonData.name,
+        pokemonId: pokemonData.id,
+        pokemonType1: pokemonData.types[1].type.name,
+        pokemonType2: pokemonData.types[0].type.name,
+        pokemonImgUrl: pokemonData.sprites.front_default
       });
     });
   }
@@ -27,6 +35,9 @@ class App extends React.Component {
       <div className="pokemon-info">
         <h1>Pokedex</h1> 
         <h2>Name: { this.state.selectedPokemon }</h2>
+        <h2>Entry#: { this.state.pokemonId }</h2>
+        <h2>Type1: { this.state.pokemonType1 } Type2: { this.state.pokemonType2 }</h2>
+        <img className="pokemon-image" src={this.state.pokemonImgUrl}/>
       </div> 
     );
   }
