b7376bd1d6d53f00c489357f1b7c739f2a84f444
clarkgh <demolisho@hotmail.com>
2017-10-05 09:05:12 -0700

    sparts project

---
+++ sparts/App.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return ( <div>Do you like da sparts? I love da sparts.</div> );
+  }
+}
+
+export default App;
