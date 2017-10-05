158a39c59d208b54ef3590266e30fcfb4b286275
clarkgh <demolisho@hotmail.com>
2017-10-05 10:25:01 -0700

    setup for challenge

--- sparts/main.js
+++ sparts/main.js
@@ -4,7 +4,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+
+const fetchUser = (email) => {
+  // Implement this
+}
+
+class UserInfo extends React.Component{
+  // Implement this
+}
+
+class UserCard extends React.Component{
+  // Implement this
+}
+
+class NotFound extends React.Component{
+  // Implement this
+}
+
+const userData = {
+  'scurry@warriors.com': {
+    firstName: 'Steph',
+    lastName: 'Curry',
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnHBqYZ7nE0Bo6PcpzHbo4ONXpSXkG8kzI8rSUhTvm2LP6DHm9',
+  },
+  'kdurant@warriors.com': {
+    firstName: 'Kevin',
+    lastName: 'Durant',
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTffO_a5ljAYdZaqtiFgHVQ_F3pIwChM8wOvhBOzt-O1yZq8-Pbvg',
+  },
+}
+
+// DO NOT CHANGE BELOW
+React.render(
+  <UserInfo email="scurry@warriors.com">
+    {(user, changeUser) => user === null ?
+      <NotFound /> : <UserCard user={user} changeUser={changeUser} />}
+  </UserInfo>,
+  document.getElementById('container')
 );
