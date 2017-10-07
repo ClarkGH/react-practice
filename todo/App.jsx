94150f62bc7f91b612ca21b027b8f1f9b4508e41
clarkgh <demolisho@hotmail.com>
2017-10-07 14:05:41 -0700

    set up for first component

--- todo/App.jsx
+++ todo/App.jsx
@@ -3,6 +3,16 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      items: [],
+    }
+    
+  }
+
   render() {
     return ( <div>We Gonna TODO a todo, cuz basics</div> );
   }
