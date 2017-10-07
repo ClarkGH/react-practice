349cb7d8073cf75ec02bd1145c1afc699388e2ca
clarkgh <demolisho@hotmail.com>
2017-10-07 13:25:17 -0700

    Let's react to a todo list

---
+++ todo/App.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return ( <div>We Gonna TODO a todo, cuz basics</div> );
+  }
+}
+
+export default App;
