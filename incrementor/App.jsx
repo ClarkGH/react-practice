a1fe1b17fb710f44f400e76ad095ffe48896f12a
clarkgh <demolisho@hotmail.com>
2017-10-09 12:33:41 -0700

    fixed syntax

--- incrementor/App.jsx
+++ incrementor/App.jsx
@@ -7,8 +7,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicks = 0,
-      show = true
+      clicks: 0,
+      show: true
     };
   }
 
@@ -31,7 +31,7 @@ class App extends React.Component {
   }
 
   render() {
-    return ();
+    return (<div></div>);
   }
 }
 
