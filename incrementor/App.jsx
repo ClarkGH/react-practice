2dba6457b3bab3f9c3876becc0e02e7635268346
clarkgh <demolisho@hotmail.com>
2017-10-09 12:27:28 -0700

    Component and methods

---
+++ incrementor/App.jsx
@@ -0,0 +1,38 @@
+'use strict';
+
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks = 0,
+      show = true
+    };
+  }
+
+  decrement() {
+    this.setState({
+      clicks: this.state.clicks - 1
+    });
+  }
+
+  increment() {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  }
+
+  toggle() {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+  render() {
+    return ();
+  }
+}
+
+export default App;
