58b377593c8a023416a4b23729c8a0575746d2bf
clarkgh <demolisho@hotmail.com>
2017-10-14 16:48:16 -0700

    serving json

--- js-developer-test-1.0.0/api/index.js
+++ js-developer-test-1.0.0/api/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+const express = require('express'),
+  app = express(),
+  fs = require('fs'),
+  manifest = require('./manifest.json');
+
+app.get('/manifest', (req, res) => {
+  res.send(manifest);
+});
 
 module.exports = app;
