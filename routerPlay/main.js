1a5c29c1776b8a90c74ed3588093a6381333c824
clarkgh <demolisho@hotmail.com>
2017-10-31 20:33:53 -0700

    Transitioned to react router, error: 'cannot read property Router of undefined', troubleshooting

--- routerPlay/main.js
+++ routerPlay/main.js
@@ -2,11 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactRouter from 'react-router';
 import App from './components/App.js';
 
 ReactDOM.render(
-  <div>
-    <App />
-  </div>,
+  <ReactRouter.Router history={ReactRouter.hashHistory}>
+    <ReactRouter.Route path="/" component={App}>
+ 
+    </ReactRouter.Route>
+  </ReactRouter.Router>,
   document.getElementById('root')
 );
