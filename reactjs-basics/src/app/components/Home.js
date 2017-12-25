c21168e6b37d9f4e972ee0efd615f49ba87fca1c
clarkgh <demolisho@hotmail.com>
2017-12-24 21:35:09 -0800

    events

--- reactjs-basics/src/app/components/Home.js
+++ reactjs-basics/src/app/components/Home.js
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
+  constructor(props) {
+    super();
+    this.age = props.age;
+  }
+
+  onMakeOlder() {
+    this.age++;
+  }
   // props stored in this.props
   render() {
     return(
       <div>
-        <p>Oh hai, {this.props.name} ({this.props.user.realName}), you {this.props.user.age} year old bastard!</p>
-        <h4>I hear you like the following:</h4>
-        <div>
-          <ul>
-            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-          </ul>
-        </div>
-        {this.props.children}
+        <p>Oh hai, {this.props.name} you are {this.age}</p>
+        <br>
+        <button onClick={onMakeOlder.bind(this)} className="btn btn-primary"></button>
       </div>
     );
   }
@@ -21,7 +24,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: PropTypes.string,
-  age: PropTypes.number,
-  user: PropTypes.object,
-  children: PropTypes.element.isRequired
+  age: PropTypes.number
 }
