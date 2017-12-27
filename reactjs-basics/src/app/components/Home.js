15df5cc39749fe6a36abeb9ec0cfa10301b79249
clarkgh <demolisho@hotmail.com>
2017-12-26 16:23:30 -0800

    state

--- reactjs-basics/src/app/components/Home.js
+++ reactjs-basics/src/app/components/Home.js
@@ -4,19 +4,23 @@ import PropTypes from 'prop-types';
 export class Home extends React.Component {
   constructor(props) {
     super();
-    this.age = props.age;
+    this.state = {
+      age: props.initialAge //only set state as props on intialisation
+    };
   }
 
   onMakeOlder() {
-    this.age++;
+    this.setState({
+      age: this.state.age++;
+    });
   }
   // props stored in this.props
   render() {
     return(
       <div>
-        <p>Oh hai, {this.props.name} you are {this.age}</p>
+        <p>Oh hai, {this.props.name} you are {this.state.age}</p>
         <br>
-        <button onClick={onMakeOlder.bind(this)} className="btn btn-primary"></button>
+        <button onClick={() => this.onMakeOlder())} className="btn btn-primary"></button>
       </div>
     );
   }
@@ -24,5 +28,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: PropTypes.string,
-  age: PropTypes.number
+  initialAge: PropTypes.number
 }
