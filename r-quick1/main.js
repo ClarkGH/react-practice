c7cd8baeeb0849a3eba7ddd50caa231e88471986
clarkgh <demolisho@hotmail.com>
2018-01-08 21:03:51 -0800

    extra elements with wonky js

--- r-quick1/main.js
+++ r-quick1/main.js
@@ -1,6 +1,6 @@
 const h1 = React.createElement('h1', null, 'Hello World');
 
 ReactDOM.render(
-  h1,
+  React.createElement('div', null, h1, h1), //extra elements
   document.getElementById('container')
 );
