7b6e51e61ddd6354dd6fce19d6a632e93cf524ab
clarkgh <demolisho@hotmail.com>
2018-01-08 21:28:21 -0800

    props with vanilla js

--- r-quick1/main.js
+++ r-quick1/main.js
@@ -2,11 +2,42 @@ const h1 = React.createElement('h1', null, 'Hello World');
 
 class HelloWorld extends React.Component {
   render() {
-    return React.createElement('div', null, h1, h1);
+    return React.createElement(
+      'h1', 
+      null, 
+      'Hello ' + this.props.frameworkName + ' world!'
+    );
   }
 }
 
 ReactDOM.render(
-  React.createElement(HelloWorld, null),
+  React.createElement( 
+    'div',
+    null,
+    React.createElement(
+      HelloWorld,
+      {
+        id: 'ember',
+        frameworkName: 'Ember.js',
+        title: 'This is the last framework your should touch'
+      }
+    ),
+    React.createElement(
+      HelloWorld,
+      {
+        id: 'backbone',
+        frameworkName: 'Backbone.js',
+        title: 'Meh'
+      }
+    ),
+    React.createElement(
+      HelloWorld,
+      {
+        id: 'React',
+        frameworkName: 'React.js',
+        title: 'This isn\'t too bad'
+      }
+    )
+  ),
   document.getElementById('container')
 );
