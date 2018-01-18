54a5e4267484a98c7a552452c4ee1692c237ab64
clarkgh <demolisho@hotmail.com>
2018-01-17 20:12:46 -0800

    Can now take screenshots

--- vidShot/js/main.js
+++ vidShot/js/main.js
@@ -24,16 +24,49 @@ navigator.mediaDevices.getUserMedia({video: true, audio: false})
     console.log(`Error: ${e}`);
   });
 
-  video.addEventListener('canplay', (e) => {
-    if (!streaming) {
-      // Set video / canvas height
-      height = video.videoHeight / (video.videoWidth / width);
+video.addEventListener('canplay', (e) => {
+  if (!streaming) {
+    // Set video / canvas height
+    height = video.videoHeight / (video.videoWidth / width);
 
-      video.setAttribute('width', width);
-      video.setAttribute('height', height);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
+    video.setAttribute('width', width);
+    video.setAttribute('height', height);
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
 
-      streaming = true;
-    }
-  }, false);
+    streaming = true;
+  }
+}, false);
+
+// Photo btn event
+photoButton.addEventListener('click', (event) => {
+  takePicture();
+
+  event.preventDefault();
+}, false);
+
+// TODO: Filter event
+
+function takePicture() {
+  // Create canvas
+  const context = canvas.getContext('2d');
+  if(width && height) {
+    // set canvas properties
+    canvas.width = width;
+    canvas.height = height;
+    // draw video image on canvas
+    context.drawImage(video, 0, 0, width, height);
+
+    // Create img from canvas
+    const imgUrl = canvas.toDataURL('image/png');
+
+    // Create img element
+    const img = document.createElement('img');
+
+    // Set img src
+    img.setAttribute('src', imgUrl);
+
+    // Add image to photos
+    photos.appendChild(img);
+  }
+}
