6c8df32c540b37eace76b76755f4789bf718264b
clarkgh <demolisho@hotmail.com>
2018-01-20 15:26:16 -0800

    first next.js app

---
+++ next-practice/.next/dist/pages/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/bagman/Coding/react-practice/next-practice/pages/index.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    }
+  }, "Hello world");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7OztrQkFBZSxZQUFBO3lCQUFPLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxHQUFBLEVBQVAsQUFBTztBQUF0QiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvYmFnbWFuL0NvZGluZy9yZWFjdC1wcmFjdGljZS9uZXh0LXByYWN0aWNlIn0=
