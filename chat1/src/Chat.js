c494628c84f9aa0c069cd6afa8d7ebe6b16e83f1
clarkgh <demolisho@hotmail.com>
2018-03-28 16:00:37 -0700

    mock data, submit functionality, and i like turtles with each new message + scrolling on update/mount

--- chat1/src/Chat.js
+++ chat1/src/Chat.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Message from './Message';
 
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chats: [{
+        username: "Batman",
+        content: <p>WHERE IS HE!</p>,
+      }, {
+        username: "Robin",
+        content: <p>Where is who?</p>,
+      }, {
+        username: "Batman",
+        content: <p>You're gonna love me!</p>
+      }, {
+        username: "Robin",
+        content: <p>Holy geometry, batman.</p>
+      }, {
+        username: "Batman",
+        content: <p>I liek chocolate milk!</p>
+      }, {
+        username: "Robin",
+        content: <p>Have you gone mad?</p>
+      }, {
+        username: "Batman",
+        content: <p>*Eats a pear*</p>
+      }, {
+        username: "Robin",
+        content: <p>...</p>
+      }]
+    };
+
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+
+  componentDidMount() {
+    this.scrollToBot();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBot();
+  }
+
+  scrollToBot() {
+    ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(this.refs.chats).scrollHeight;
+  }
+
+  submitMessage(e) {
+    e.preventDefault();
+
+    this.setState({
+      chats: this.state.chats.concat([{
+        username: "Batman",
+        content: <p>{ReactDOM.findDOMNode(this.refs.msg).value}</p>
+      }])
+    }, () => {
+      ReactDOM.findDOMNode(this.refs.msg).value = "";
+    });
+  }
+
   render() {
+    const username = "Batman";
+    const { chats } = this.state;
+
     return (
-      <div className="Chat">
-        <h1>Chat yourself</h1>
-        
+      <div className="chatroom">
+        <h3>Chilltime</h3>
+        <ul className="chats" ref="chats">
+          {
+            chats.map((chat) =>
+              <Message chat={chat} user={username} />
+            )
+          }
+        </ul>
+        <form className="input" onSubmit={(e) => this.submitMessage(e)}>
+          <input type="text" ref="msg" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-    )
+    );
   }
 }
 
---
+++ chat1/src/Message.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <div className="Message">
+        <h1>I like turtles</h1>
+      </div>
+    )
+  }
+}
+
+export default Message;
