89e5fddade2ace33ee47bb14a23f4bef5747c5d8
clarkgh <demolisho@hotmail.com>
2018-04-15 13:44:57 -0700

    tooltip created

--- tooltip/src/main.js
+++ tooltip/src/main.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    Say that we wanted a custom tooltip, <App text='such magic, such javascript'> It could look like this.</App>
+  </div>,
   document.getElementById('root'),
 );
