6162079f2b894f9f8533555dd7c33294065fcee5
clarkgh <demolisho@hotmail.com>
2018-06-17 20:06:20 -0700

    cloned boilerplate to start doggy daycare locator application

---
+++ doggy_daycare_locator/src/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App/App';
+
+console.log("Hello world.");
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
