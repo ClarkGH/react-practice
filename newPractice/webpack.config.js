86f019e418282cb5f601835ee74bdccf41c9331f
clarkgh <demolisho@hotmail.com>
2018-07-08 13:58:13 -0700

    css in js loaders added

--- newPractice/webpack.config.js
+++ newPractice/webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, exclude: /node_modules/, use: ['style-loader', 'css-loader'] }
     ]
   },
 };
