a11bb8fe70587bc96b5f5dfedb33f0d3005f5997
clarkgh <demolisho@hotmail.com>
2018-07-09 16:16:40 -0700

    router created

--- router/src/App/App.js
+++ router/src/App/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Router from './Components/Router';
+
+const mapping = {
+  '#projects': <div>Projects (<a href="#">home</a>)</div>,
+  '#about': <div>About Me (<a href="#">home</a>)</div>,
+  '*': <div>Dashboard
+    <br/>
+    <a href="#projects">Projects</a>
+    <br/>
+    <a href="#about">About</a>
+    </div>
+}
 
 const App = (props) => {
-  return(<h1>Hello World</h1>);
+  return(<Router mapping={mapping} />);
 }
 
 export default App;
