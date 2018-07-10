d278b3e7bd68bf7f01ae6e2038a8c95f709649c8
clarkgh <demolisho@hotmail.com>
2018-07-09 20:41:01 -0700

    routes setup and about component

--- react-router/src/App/App.js
+++ react-router/src/App/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Router, Route, Link } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
+
+// Components for routes
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Content from './Components/Content';
+import Post from './Components/Post';
+import Posts from './Components/Posts';
+import Login from './Components/Login';
 
 const App = () => {
-  return(<h1>Hi mom!</h1>);
+  return(
+    <Router history={hashHistory}>
+      <Route path="/" component={Content}>
+        <Route path="/about" component={About} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/posts/:id" component={Post} />
+        <Route path="/contact" component={Contact} />
+      </Route>
+      <Route path="/login" component={Login} />      
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-router/src/App/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hibbity doobity, it's all about the doobity</h1>
+    </div>
+  );
+}
+
+export default About;
