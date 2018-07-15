7e91a87991e9feb3e1bf911a18af232ac24e3c1f
clarkgh <demolisho@hotmail.com>
2018-07-14 18:36:44 -0700

    form

--- weatherApp/src/App/App.js
+++ weatherApp/src/App/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
+// API Key
+import API_KEY from '../secret';
+
 class App extends React.Component {
   constructor (props) {
     super (props);
+    this.state = { 
+      city: 'San Francisco',
+      state: 'CA'
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    // this.setState({
+    // });
+  }
+
+  handleSubmit(event) {
+    alert('submission passed');
   }
 
   render () {
-    return (<h1> Hello World.</h1>);
+    return (
+      <form> 
+        <label>City</label>
+        <input type='text' name='city' onChange={this.handleChange} value={this.state.city} />
+
+        <label>City</label>
+        <input type='text' name='state' onChange={this.handleChange value={this.state.city}} />        
+        <input type='submit' value="submit" />
+      </form>
+    );
   }
 }
 
