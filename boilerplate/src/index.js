8e1da5ad4bf78537849a720305e14cbc60733a9d
clarkgh <demolisho@hotmail.com>
2018-10-16 12:29:06 -0700

    webpack added

---
+++ boilerplate/src/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+function component() {
+  let el = document.createElement('h1');
+  el.innerHTML = _.join(['GREETINGS', 'MEATBAGS'], ' ');
+
+  return el;
+}
+
+document.body.appendChild(component());
