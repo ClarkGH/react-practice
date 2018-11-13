1073cc2d007a542cfce39147e4df759c4e1cd755
clarkgh <demolisho@hotmail.com>
2018-11-13 11:25:29 -0700

    boilerplate basics

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,14 +1,23 @@
-const path = require('path');
-
 module.exports = {
   entry: './src/main.js',
-  output: {
-    filename: './js/app.bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
     ]
   },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist/js',
+    publicPath: '/',
+    filename: 'app.bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
 };
